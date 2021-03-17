ba2e33774d4b96ea3a6c9105b3e37cae61e2b13c
solk <isolk@qq.com>
2021-03-17 18:46:41 +0800

    读取硬盘并读取elf尝试

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@ rm src/boot.o
 
 mkfile -n 1M bochs/disk.img
 nasm src/boot.asm -o src/boot.o
+nasm src/kernel.asm -o src/kernel.o
+
 dd bs=512 count=1 if=src/boot.o of=bochs/disk.img seek=0 conv=notrunc
+dd bs=512 count=1 if=src/kernel.o of=bochs/disk.img seek=1 conv=notrunc
 bochs -qf bochs/bochs.cfg
