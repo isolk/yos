fd61e6194e8577180bd9034adf40723f68bc0e1c
solk <isolk@qq.com>
2021-03-19 17:42:36 +0800

    历经千辛万苦,终于可以正确编译运行c语言编写的hello,world. 稍后整理文件夹,先提交保存

--- learn/main.c
+++ learn/main.c
@@ -1,11 +1,11 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-uint8_t *bf;
+//uint8_t *bf;
 int _start()
 {
-    bf = (uint8_t)0xb8000;
-    bf[0] = 'h';
-    bf[1]=0x70;
+    uint8_t *pf = (uint8_t*)0xB8000;
+    *pf = 'x';
+    for(;;){}
     return 0;
 }
