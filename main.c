dcc20c80b6d5d6df4b34b9206cd74b4b4a616dc0
solk <isolk@qq.com>
2021-03-19 17:42:36 +0800

    历经千辛万苦,终于可以正确编译运行c语言编写的hello,world. 稍后整理文件夹,先提交保存

--- main.c
+++ main.c
@@ -1,11 +1,9 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-uint8_t *bf;
+//uint8_t *bf;
 int _start()
 {
-    bf = (uint8_t)0xb8000;
-    bf[0] = 'h';
-    bf[1]=0x70;
+    *((int*)0xb8000)=0x07690748;
     return 0;
 }
--- tmp/loader.asm
+++ tmp/loader.asm
@@ -1,3 +1,4 @@
+;------------------------------读取内核镜像--------------------------
 ; 1 boot.asm 2-3 loader.asm 4 kernel.c 的第一个扇区
 mov ah,0x02    ; 表示读取
 mov al,1       ; 要读取扇区数
@@ -36,7 +37,7 @@ cmp dx,0 ; 如果余数为0，给商加1
 jz cmm   
   inc ax
 cmm: cmp ax,0
-jz over
+jz read_over
   mov ah,0x02    ; 表示读取
   ;mov al,al      ; 要读取扇区数
   mov ch,0       ; 磁道号
@@ -48,4 +49,74 @@ jz over
   mov es,bx      ; 
   mov bx,0       ; es:bx是数据在内存的缓存地址
   int 0x13       ; 磁盘中断
-over: hlt
+
+read_over: 
+
+;---------------------进入保护模式----------------------------
+
+;创建两个GDT段，全局代码段和显存段。
+gdt_addr equ 0x6000
+
+; 强制要求空段
+mov dword [gdt_addr],0x00
+mov dword [gdt_addr+4],0x00
+
+; 代码段,0 4G
+mov dword [gdt_addr+8],0x00_00_ff_ff
+mov dword [gdt_addr+12],0x00_cf_98_00
+
+; 数据段 0,4G
+mov dword [gdt_addr+16],0x00_00_ff_ff
+mov dword [gdt_addr+20],0x00_cf_92_00
+
+lgdt [0x7e00+gdt_size]
+
+in al,0x92
+or al,0000_0010B
+out 0x92,al
+
+cli
+
+mov eax,cr0
+or eax,1
+mov cr0,eax
+
+jmp dword 0x0008:flush+0x7e00
+
+[bits 32]
+flush:
+mov cx,00000000_000_10_000b
+mov ds,cx
+mov es,cx
+mov ebx,0xb8000
+mov byte [ebx+0x00],'l'
+mov byte [ebx+0x02],'o'
+mov byte [ebx+0x04],'d'
+mov byte [ebx+0x06],'d'
+mov byte [ebx+0x08],'i'
+mov byte [ebx+0x0a],'g'
+
+;------------------加载内核代码段----------------------
+; 0x34是程序段首地址，4字节
+;mov eax,0x18    ; 程序入口地址 在内存中的地址
+
+;mov bx,0x8200+0x34
+;mov eax,[bx+0c] ; 内存重定位地址
+;mov eax,[bx+04] ; 文件偏移
+;mov eax,[bx+14] ; 程序长度
+;mov eax,[bx+20] ; 字节对齐
+
+mov bx,0x8200+0x34
+cld
+mov ecx,[bx+0x10] ; 文件中长度
+mov esi,[bx+0x04] ; 文件偏移
+add esi,0x8200
+mov edi,[bx+0x08] ; 重定位内存地址 
+rep movsb 
+
+jmp [0x8200+0x18]
+
+hlt
+
+gdt_size dw 23
+         dd 0x006000
