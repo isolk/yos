871c9f5a024705ee195d84a9cfdacab27a7d1a58
yanghaitao <yanghaitao@yanghaitaodeMacBook-Pro.local>
2021-03-21 00:33:10 +0800

    添加make自动编译；新增光标操作，以实现c调用汇编

--- src/kernel.c
+++ src/kernel.c
@@ -1,16 +1,50 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+void set_cur();
+
+size_t g_row=0;
+size_t g_column=0;
+void put_char(size_t row,size_t column,uint8_t str){
+    uint8_t* tr = (uint8_t*)0xb8000;
+    tr = tr+row*160+column*2;
+    *tr = str;
+}
+
+void clear(){
+    for(size_t i=0;i<25;i++){
+        for (size_t j=0;j<80;j++){
+            put_char(i,j,' ');
+        }
+    }
+}
+
+void print_char(uint8_t str){
+    if (str == '\n'){
+        g_row++;
+        g_column=0;
+        put_char(g_row,g_column,str);
+        return;
+    }
+
+    put_char(g_row,g_column,str);
+    g_column++;
+    if(g_column>=80){
+        g_row++;
+        g_column=0;
+    }
+}
+
 int _start()
 {
-    uint8_t *pf = (uint8_t*)0xB8000;
-    char *s = "hello,world";
-    for (size_t i = 0; i < 11; i++)
+    clear();
+    char *s = "hello,world.\nwhat you are!!!!!";
+    for (size_t i = 0; i < 20; i++)
     {
-        *pf = s[i];
-        pf +=2;
+        print_char(s[i]);
     }
     
+    set_cur();
     for(;;){}
     return 0;
 }
