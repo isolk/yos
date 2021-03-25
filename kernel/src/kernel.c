f95705dff9a5b9116f9f45ff46e458758a49a35b
solk <isolk@qq.com>
2021-03-25 12:49:52 +0800

    实现输出滚屏

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -3,13 +3,16 @@
 int _start()
 {
     clear();
-    char r[1111];
+    char r[25*80];
     for (size_t i = 0; i < sizeof(r); i++)
     {
         r[i]=i%10+'0';
     }
     
+    char * hello = "hello\0";
+    print_string(hello,strlen(hello));
     print_string(r,sizeof(r));
+    print_string(hello,strlen(hello));
 
     for(;;){}
     return 0;
--- system/src/terminal/clear.c
+++ system/src/terminal/clear.c
@@ -1,7 +1,10 @@
 #include<terminal.h>
-void clear(){
-    for(size_t i=0;i<ROW;i++){
-        for (size_t j=0;j<WIDTH;j++){
+void clear()
+{
+    for(size_t i=0;i<ROW;i++)
+    {
+        for (size_t j=0;j<WIDTH;j++)
+        {
             put_char(i,j,' ');
         }
     }
