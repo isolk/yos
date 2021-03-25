3476ebc9c5a49ea2db8f4d19fc8c600533a16e86
solk <isolk@qq.com>
2021-03-25 12:49:52 +0800

    实现输出滚屏

--- system/include/terminal.h
+++ system/include/terminal.h
@@ -7,6 +7,7 @@ void print_string(char* str,size_t len);
 void set_cur(uint16_t postion);
 void outb(uint16_t port,uint8_t value);
 uint8_t inb(uint16_t port);
+uint8_t get_char(size_t row,size_t column);
 
 #define ROW 25
 #define WIDTH 80
--- system/src/terminal/print_char.c
+++ system/src/terminal/print_char.c
@@ -1,36 +1,50 @@
-#include<terminal.h>
+#include <terminal.h>
 int g_row;
 int g_column;
-
-void set0(size_t row)
+void moveup_row(size_t row)
 {
+    if (row == 0)
+    {
+        return;
+    }
+
     for (size_t i = 0; i < WIDTH; i++)
     {
-        put_char(row,i,'=');
+        put_char(row - 1, i, get_char(row, i));
     }
-    
+
+    if (row == ROW - 1)
+    {
+        for (size_t i = 0; i < WIDTH; i++)
+        {
+            put_char(row, i, '\0');
+        }
+    }
 }
 
 void srcoll()
 {
-    set0(4);
+    for (size_t i = 1; i < ROW; i++)
+    {
+        moveup_row(i);
+    }
 }
 
-void print_char(uint8_t str){
+void print_char(uint8_t str)
+{
     // row最大只能为24，此时位于屏幕最下方1行
     // column最大只能为79，此时位于最右侧
-    if (g_row == 4 && g_column == WIDTH-1)
+    if (g_row == ROW && g_column == 0)
     {
         srcoll();
-    }else 
-    {
-        put_char(g_row,g_column,str);
+        g_row = ROW - 1;
     }
-     
+
+    put_char(g_row, g_column, str);
     g_column++;
-    if(g_column ==WIDTH){
+    if (g_column == WIDTH)
+    {
         g_row++;
-        g_column=0;
+        g_column = 0;
     }
 }
-
