f70aa7ad920363706f0034a84f91b62172331c14
isolk <yanghaitao@yanghaitaodeMacBook-Pro.local>
2021-03-28 17:54:52 +0800

    修改时间中断处理为显示当前时间

--- kernel/src/time.c
+++ kernel/src/time.c
@@ -6,6 +6,12 @@
 void init_time()
 {
     cli();
+    //write_port_b(0x70,0x8a);
+    //write_port_b(0x71,0x2e);
+
+    //write_port_b(0x70,0x8b);
+    //write_port_b(0x71,0x42);
+
     write_port_b(0x70,0x8b);
     write_port_b(0x71,0x12);
     write_port_b(0x70,0x0c);
@@ -20,9 +26,37 @@ void init_time()
 void time_handler()
 {
     /* Send End of Interrupt (EOI) to master PIC */
-    print_char('#');
+    //print_char('#');
+
+    write_port_b(0x70,0x80);
+    uint8_t sec = read_port_b(0x71);
+
+
+    write_port_b(0x70,0x82);
+    uint8_t min = read_port_b(0x71);
+
+    write_port_b(0x70,0x84);
+    uint8_t hour = read_port_b(0x71);
+
     write_port_b(0x70,0x0c);
     read_port_b(0x71);
+
+    uint8_t h = hour>>4;
+    uint8_t l = hour&0x0F;
+    put_char(0,0,h+'0');
+    put_char(0,1,l+'0');
+    put_char(0,2,':');
+
+    h = min>>4;
+    l = min&0x0F;
+    put_char(0,3,h+'0');
+    put_char(0,4,l+'0');
+    put_char(0,5,':');
+
+    h = sec >>4;
+    l = sec&0x0F;
+    put_char(0,6,h+'0');
+    put_char(0,7,l+'0');
 
     write_port_b(0x20, 0x20);
     write_port_b(0xa0, 0x20);
